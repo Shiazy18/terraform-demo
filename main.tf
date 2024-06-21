18280eeb2eafb6d2a3535d01bb1ab8f19fe94f02
Shivam <shivampant0998@gmail.com>
2024-06-22 02:02:51 +0530

    added files in main

--- main.tf
+++ main.tf
@@ -2,3 +2,11 @@ resource "local_file" "cyberpunk" {
   filename     = var.filename
   content  = var.content
 }
+
+resource "local_file" "variabledemo" {
+
+  for_each = var.listdemo
+  filename = each.value
+  content = each.key
+  
+}
